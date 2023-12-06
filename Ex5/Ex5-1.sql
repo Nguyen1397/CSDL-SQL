9210252f6a77f1fc64739d22d0bd53e1e3143f05
Nguyen1397 <knguyen4582@gmail.com>
2023-12-06 08:51:27 +0700

    Update Ex5-1.sql

--- Ex5/Ex5-1.sql
+++ Ex5/Ex5-1.sql
@@ -10,3 +10,27 @@ CREATE TABLE LopHoc(
 	MaLopHoc INT PRIMARY KEY IDENTITY,
 	TenLopHoc VARCHAR(10)
 )
+GO
+SELECT * FROM LopHoc
+GO
+INSERT INTO LopHoc(TenLopHoc) VALUES ('T2009M')
+SELECT * FROM LopHoc
+UPDATE LopHoc SET TenLopHoc = 'T2308M'
+	WHERE MaLopHoc = 1002
+DELETE FROM LopHoc WHERE MaLopHoc = 1002
+GO
+
+DROP TABLE SinhVien
+CREATE TABLE SinhVien(
+	MaSV int PRIMARY KEY,
+	TenSV nvarchar(250),
+	ClassCode int,
+	CONSTRAINT fk FOREIGN KEY (ClassCode) REFERENCES LopHoc(MaLopHoc)
+)
+GO
+INSERT INTO SinhVien(MaSV, TenSV, ClassCode) VALUE (1,'Minh', 5);
+INSERT INTO SinhVien(MaSV, TenSV, ClassCode) VALUE (3,'Hoa', 1004);
+INSERT INTO SinhVien(MaSV, TenSV, ClassCode) VALUE (14,'Minh', 1007),(16,'Linh',1006);
+SELECT * FROM SinhVien
+SELECT * FROM LopHoc
+GO
